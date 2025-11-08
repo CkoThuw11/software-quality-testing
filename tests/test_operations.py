1d21dc6c5c382a574a0b1af1fb1772184b7956c2
ADMIN <thethu001@gmail.com>
2025-11-08 15:29:40 +0700

    fix subtract function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,7 @@
-from calculator.operations import add
+from calculator.operations import add, subtract
 
 def test_add():
     assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(4, 3) == 1
