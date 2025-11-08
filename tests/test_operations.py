f29b11dc6721b287d38fd0de22e2cc7887b977ca
ADMIN <thethu001@gmail.com>
2025-11-08 15:47:45 +0700

    divide function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-from calculator.operations import add, subtract, multiply
+from calculator.operations import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
@@ -8,3 +8,6 @@ def test_subtract():
 
 def test_multiply():
     assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(8, 4) == 2
