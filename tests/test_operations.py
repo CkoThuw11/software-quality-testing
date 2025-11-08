1a1162dd74f3a8d73683da4f4526bcb2c5ab6bc1
ADMIN <thethu001@gmail.com>
2025-11-08 15:51:29 +0700

    fix divide function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-from calculator.operations import add, subtract, multiply
+from calculator.operations import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
@@ -8,3 +8,9 @@ def test_subtract():
 
 def test_multiply():
     assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(8, 2) == 4
+
+def test_divide_zero():
+    assert divide(5, 0) == "Error: Division by zero"
